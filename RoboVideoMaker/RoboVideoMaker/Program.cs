e9af8251fd2c8342e5a7eb62488cc2d6a9686180
Tuffi Saliba Neto <tuffisal@gmail.com>
2019-05-04 13:01:10 -0300

    Add searchTerm and prefix

--- RoboVideoMaker/RoboVideoMaker/Program.cs
+++ RoboVideoMaker/RoboVideoMaker/Program.cs
@@ -4,9 +4,40 @@ namespace RoboVideoMaker
 {
   class Program
   {
+    static string AskAndReturnSearchTerm()
+    {
+      /*Console.WriteLine("Informe o termo a ser buscado na wikipedia para geração do vídeo: ");
+      return Console.ReadLine();*/
+      return "Atlético Mineiro";
+    }
+
+    static string AskAndReturnPrefix()
+    {
+      /*do
+      {
+        Console.WriteLine($"Informe o prefixo:{Environment.NewLine}[1]: Who is{Environment.NewLine}[2]: What is{Environment.NewLine}[3]: The history of{Environment.NewLine}[4]: Cancelar");
+        int option;
+        if ((!int.TryParse(Console.ReadLine(), out option)) || (option <= 0) || (option > 4))
+          Console.WriteLine("Opção inválida!");
+        else
+        {
+          switch (option)
+          {
+            case 1: return "Who is";
+            case 2: return "What is";
+            case 3: return "The history of";
+            case 4: Environment.Exit(0); break;
+            default: Console.WriteLine("Opção inválida!"); break;
+          }
+        }
+      } while(true);*/
+      return "This history of";
+    }
+
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      new VideoMakerRobot(new Content { SearchTerm = AskAndReturnSearchTerm(), Prefix = AskAndReturnPrefix() }).Start();
     }
   }
+
 }
---
+++ RoboVideoMaker/RoboVideoMaker/VideoMakerRobot.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoboVideoMaker
+{
+  public class VideoMakerRobot
+  {
+    private Content content;
+
+    public VideoMakerRobot(Content content)
+    {
+      this.content = content;
+    }
+    public void Start()
+    {      
+      Console.WriteLine($"Assunto do vídeo: {JsonConvert.SerializeObject(content)}");
+    }
+  }
+
+  public class Content
+  {
+    public string SearchTerm { get; set; }
+    public string Prefix { get; set; }
+  }
+}
